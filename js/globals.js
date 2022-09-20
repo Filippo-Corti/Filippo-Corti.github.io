a4f68f506f42edde4903f20e091702e341477b22
Filippo <corti.filippo03@gmail.com>
2022-09-20 18:46:16 +0200

    briccola spa

--- js/globals.js
+++
@@ -1,8 +0,0 @@
-export let globals = {
-    TOTAL_ROWS: 15,  /* IMPORTANT: Also Change the number of columns of the board grid in _board.scss */
-    TOTAL_COLS: 15,
-    DEFAULT_SNAKE_LENGTH: 4,
-    DEFAULT_ROW: 7,
-    DEFAULT_COL: 2,
-    PACE: 225,
-}
--- js/main.js
+++ js/main.js
@@ -1,94 +1,19 @@
-import GUI from './GUI.js';
-import SnakeGame from './SnakeGame.js';
-import { globals as G } from './globals.js';
+//Mobile Menu
+const mobileMenuButton = document.querySelector(".navbar-mobile-button");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuOpen = document.querySelector("#mobile-menu-open");
+const mobileMenuClose = document.querySelector("#mobile-menu-close");
 
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.toggleAttribute("showing");
+    mobileMenuClose.toggleAttribute("overlay");
+    mobileMenuOpen.toggleAttribute("overlay");
+});
 
-/* Create Game Board and Interface and link them together */
-const gui = new GUI(G.TOTAL_ROWS, G.TOTAL_COLS);
-const snakeGame = new SnakeGame(gui);
-gui.linkGameLogic(snakeGame);
+//Language Modal
+const languageButton = document.querySelector(".language-button");
+const languageModal = document.querySelector(".modal-language");
 
-document.querySelector(".game-buttons__start").addEventListener('click', () => {
-    snakeGame.reset();
-    gui.startGame();
-})
-
-
-/* Settings */
-
-const settingsButton = document.querySelector(".game-settings__button");
-
-settingsButton.addEventListener('click', () => {
-    document.querySelector("#game-settings").showModal();
-})
-
-const settingsSaveButton = document.querySelector(".game-settings__save");
-
-settingsSaveButton.addEventListener('click', () => {
-    let newPace = 450 - document.querySelector("#game-speed").value;
-    document.querySelector("#game-settings").close();
-    G.PACE = newPace;
-})
-
-/* Close Game Over Button*/
-
-const closeGameOver = document.querySelector(".game-over__button");
-
-closeGameOver.addEventListener('click',() => {
-    document.querySelector("#game-over").close();
-})
-
-
-/* Mobile Controls */
-
-
-// CONTROLS FOR SCROLL-MOVING
-/* document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;
-var yDown = null;
-
-function getTouches(evt) {
-    return evt.touches ||             // browser API
-        evt.originalEvent.touches; // jQuery
-}
-
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];
-    xDown = firstTouch.clientX;
-    yDown = firstTouch.clientY;
-};
-
-function handleTouchMove(evt) {
-    if (!xDown || !yDown) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-        if (xDiff > 0) {
-            // right swipe 
-            gui.mobileMove('A');
-        } else {
-            gui.mobileMove('D');
-            // left swipe 
-        }
-    } else {
-        if (yDiff > 0) {
-            // down swipe
-            gui.mobileMove('W');
-        } else {
-            gui.mobileMove('S');
-            // up swipe 
-        }
-    }
-    xDown = null;
-    yDown = null;
-};  */
-
+languageButton.addEventListener('click', () => {
+    languageModal.showModal();
+});
