ee00b1d431e33ff7aaeb20462ff075a2353578a4
Filippo <corti.filippo03@gmail.com>
2023-02-17 18:30:41 +0100

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        midnight: '#10253f'
+      },
+      listStyleType: {
+        square: 'square',
+      }
+    },
+  },
+  plugins: [],
+}
